80903017a631accadf31e65741b141fddb8f56c0
Danila Popov <danila.popov@mail.ru>
2023-08-13 23:24:22 +0400

    fix

--- lib/data/api/activation_code_api.dart
+++ lib/data/api/activation_code_api.dart
@@ -3,12 +3,12 @@ import 'package:quick_meet/data/models/server_response.dart';
 import 'package:quick_meet/data/rest_api/http_client.dart';
 
 class ActivationCodeApi {
-  static Future<ServerResponse?> confirm() async {
-    ServerResponse? res = await HTTPClient.httpPost(
-      command: '/ru/api/store/login/',
-      headers: {},
-      serverUrl: Uri.parse('${AppConstants.domain}/api/v1/auth/code/confirm'),
-    );
-    return res;
-  }
+  // static Future<ServerResponse?> confirm() async {
+  //   ServerResponse? res = await HTTPClient.httpPost(
+  //     command: '/ru/api/store/login/',
+  //     headers: {},
+  //     serverUrl: Uri.parse('${AppConstants.domain}/api/v1/auth/code/confirm'),
+  //   );
+  //   return res;
+  // }
 }
